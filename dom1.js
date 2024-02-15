d08ba00980c7514a529d922ff2333a803cdba2a1
Lyba <faisallyba06@gmail.com>
2024-02-15 12:21:53 +0500

    TagName

--- dom1.js
+++ dom1.js
@@ -5,3 +5,8 @@ document.getElementById("ptag").innerHTML= "Lyba Here From @csschuckles";
 let element= document.getElementById("ptag2");
 
 document.getElementById("demo").innerHTML= "Modern advancements are due to:" + element.innerHTML;
+
+
+let element1= document.getElementsByTagName("p");
+
+document.getElementById("demo1").innerHTML= "First message=" + element1[3].innerHTML;
