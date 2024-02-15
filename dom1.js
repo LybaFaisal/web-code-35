0d492005f69505ed8597e406644a5e422d0e7efd
Lyba <faisallyba06@gmail.com>
2024-02-15 11:54:23 +0500

    finding html element by Id

--- dom1.js
+++ dom1.js
@@ -1,1 +1,7 @@
 document.getElementById("ptag").innerHTML= "Lyba Here From @csschuckles";
+
+
+
+let element= document.getElementById("ptag2");
+
+document.getElementById("demo").innerHTML= "Modern advancements are due to:" + element.innerHTML;
